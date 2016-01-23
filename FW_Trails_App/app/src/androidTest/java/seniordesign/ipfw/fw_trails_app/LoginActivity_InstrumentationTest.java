dc68866a40cf6c5600de623bc6323dbb1a9de224
Jaron Somers <somejj01@students.ipfw.edu>
2016-01-23 17:19:56 -0500

    Adding the create new account button with tests

--- FW_Trails_App/app/src/androidTest/java/seniordesign/ipfw/fw_trails_app/LoginActivity_InstrumentationTest.java
+++ FW_Trails_App/app/src/androidTest/java/seniordesign/ipfw/fw_trails_app/LoginActivity_InstrumentationTest.java
@@ -30,6 +30,9 @@ public class LoginActivity_InstrumentationTest {
    private String usernameHint     = "Name";
    private String passwordHint     = "Password";
    private String button_OK_String = "Ok";
+   private String button_CANCEL_String = "Cancel";
+   private String textView_NEW_USER    = "New User?";
+   private String button_CreateNewAccount_String = "Create New Account";
 
    // Preferred JUnit 4 mechanism of specifying the activity to be launched before each test
    @Rule
@@ -68,11 +71,48 @@ public class LoginActivity_InstrumentationTest {
    // Verifies the Ok button exists.
    @Test
    public void okButtonExists(){
-      onView(withId(R.id.button_OK)).check(matches(isDisplayed()));
+      onView(withId(R.id.button_Ok)).check(matches(isDisplayed()));
    }
 
+   // Tests if the okay button has the correct text
    @Test
    public void okButtonTextVerification(){
-      onView(withId(R.id.button_OK)).check(matches(withText(button_OK_String)));
+      onView(withId(R.id.button_Ok)).check(matches(withText(button_OK_String)));
+   }
+
+   // Tests if the cancel button exists
+   @Test
+   public void cancelButtonExists() {
+      onView(withId(R.id.button_Cancel)).check(matches(isDisplayed()));
+   }
+
+   // Tests if the cancel button has the correct text.
+   @Test
+   public void cancelButtonVerification() {
+      onView(withId(R.id.button_Cancel)).check(matches(withText(button_CANCEL_String)));
+   }
+
+   //Tests that the text view for the new user area exists
+   @Test
+   public void newUserTextViewExists(){
+      onView(withId(R.id.textViewNewUser)).check(matches(isDisplayed()));
+   }
+
+   // Tests that the text is the correct value for the new user static text
+   @Test
+   public void newUserTextViewVerification() {
+      onView(withId(R.id.textViewNewUser)).check(matches(withText(textView_NEW_USER)));
+   }
+
+   // Tests if the create new account button exists
+   @Test
+   public void createAccountButtonExists(){
+      onView(withId(R.id.button_CreateAccount)).check(matches(isDisplayed()));
+   }
+
+   // Tests that the text is the correct value for the new account button
+   @Test
+   public void createNewAccountButtonVerification() {
+      onView(withId(R.id.button_CreateAccount)).check(matches(withText(button_CreateNewAccount_String)));
    }
 }
