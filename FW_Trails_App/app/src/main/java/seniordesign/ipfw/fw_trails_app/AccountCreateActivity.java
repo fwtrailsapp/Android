bdcde9c7d6f1207a108ba258c36075bcdee9eccb
Jaron Somers <somejj01@students.ipfw.edu>
2016-01-31 18:41:07 -0500

    Removing MainActivity and other files related to that activity for a clean slate

--- FW_Trails_App/app/src/main/java/seniordesign/ipfw/fw_trails_app/AccountCreateActivity.java
+++ FW_Trails_App/app/src/main/java/seniordesign/ipfw/fw_trails_app/AccountCreateActivity.java
@@ -11,7 +11,7 @@ public class AccountCreateActivity extends AppCompatActivity {
    protected void onCreate(Bundle savedInstanceState) {
       super.onCreate(savedInstanceState);
       setContentView(R.layout.activity_account_create);
-      getSupportActionBar().setTitle(createAcctActivityTitle);
-      getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+      //getSupportActionBar().setTitle(createAcctActivityTitle);
+     // getSupportActionBar().setDisplayHomeAsUpEnabled(true);
    }
 }
