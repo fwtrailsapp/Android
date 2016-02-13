300baa69cc2a1da78b9c80787fd66ead8768a193
Jaron Somers <somejj01@students.ipfw.edu>
2016-02-13 15:24:57 -0500

    Back pressed now works with all fragments except RecordActivityFragment

--- FW_Trails_App/app/src/main/java/seniordesign/ipfw/fw_trails_app/MainActivity.java
+++ FW_Trails_App/app/src/main/java/seniordesign/ipfw/fw_trails_app/MainActivity.java
@@ -4,6 +4,7 @@ import android.content.DialogInterface;
 import android.os.Bundle;
 
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AlertDialog;
 import android.support.design.widget.NavigationView;
@@ -41,22 +42,99 @@ public class MainActivity extends AppCompatActivity
       drawer.setDrawerListener(toggle);
       toggle.syncState();
 
+      // Add a backstack listener for when the backstack changes.
+      // Also updates the app bar titel and nav drawer with the respective new fragment.
+      setupBackStackListener();
 
       NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
       navigationView.setNavigationItemSelectedListener(this);
       displayView(R.id.nav_recordActivity);
    }
 
+   // Method creates a backstack listener and keeps the nav drawer up to date with what
+   // current fragment is displayed.
+   private void setupBackStackListener() {
+      getSupportFragmentManager().addOnBackStackChangedListener(new FragmentManager.OnBackStackChangedListener() {
+
+         @Override
+         public void onBackStackChanged() {
+            Fragment f = getSupportFragmentManager().findFragmentById(R.id.frameLayoutContent);
+            if (f != null) {
+               updateTitleAndDrawer(f);
+            }
+
+         }
+      });
+   }
+
+   // Updates the app bar title and nav drawer selected item per the fragment passed in.
+   private void updateTitleAndDrawer(Fragment fragment) {
+      String fragClassName = fragment.getClass().getName();
+      NavigationView navView = (NavigationView) findViewById(R.id.nav_view);
+
+      if (fragClassName.equals(AccountDetailsFragment.class.getName())) {
+         if (getSupportActionBar() != null) {
+            getSupportActionBar().setTitle(((AccountDetailsFragment) fragment).getTitle());
+         }
+         //set selected item position, etc
+         navView.setCheckedItem(R.id.nav_accountDetails);
+      } else if (fragClassName.equals(AccountStatisticsFragment.class.getName())) {
+         if (getSupportActionBar() != null) {
+            getSupportActionBar().setTitle(((AccountStatisticsFragment) fragment).getTitle());
+         }
+         //set selected item position, etc
+         navView.setCheckedItem(R.id.nav_accountStatistics);
+      } else if (fragClassName.equals(AchievementsFragment.class.getName())) {
+         if (getSupportActionBar() != null) {
+            getSupportActionBar().setTitle(((AchievementsFragment) fragment).getTitle());
+         }
+         //set selected item position, etc
+         navView.setCheckedItem(R.id.nav_achievements);
+      } else if (fragClassName.equals(ActivityHistoryFragment.class.getName())) {
+         if (getSupportActionBar() != null) {
+            getSupportActionBar().setTitle(((ActivityHistoryFragment) fragment).getTitle());
+         }
+         //set selected item position, etc
+         navView.setCheckedItem(R.id.nav_activityHistory);
+      } else if (fragClassName.equals(RecordActivityFragment.class.getName())) {
+         if (getSupportActionBar() != null) {
+            getSupportActionBar().setTitle(((RecordActivityFragment) fragment).getTitle());
+         }
+         //set selected item position, etc
+         navView.setCheckedItem(R.id.nav_recordActivity);
+      } else if (fragClassName.equals(TrailMapFragment.class.getName())) {
+         if (getSupportActionBar() != null) {
+            getSupportActionBar().setTitle(((TrailMapFragment) fragment).getTitle());
+         }
+         //set selected item position, etc
+         navView.setCheckedItem(R.id.nav_trailMap);
+      }
+   }
+
+   // This method replaces the current fragment with a fragment on the backstack
+   private void replaceFragment(Fragment fragment) {
+      String backStateName = fragment.getClass().getName();
+      String fragmentTag = backStateName;
+
+      FragmentManager manager = getSupportFragmentManager();
+      boolean fragmentPopped = manager.popBackStackImmediate(backStateName, 0);
+
+      if (!fragmentPopped && manager.findFragmentByTag(fragmentTag) == null) { //fragment not in back stack, create it.
+         FragmentTransaction ft = manager.beginTransaction();
+         ft.replace(R.id.frameLayoutContent, fragment, fragmentTag);
+         ft.addToBackStack(backStateName);
+         ft.commit();
+      }
+   }
+
+   // Always have at least one item on the backstack, so if we are at one,
+   // we want to exit the application, else let Android handle the back op.
    @Override
    public void onBackPressed() {
-      DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
-      if (drawer.isDrawerOpen(GravityCompat.START)) {
-         drawer.closeDrawer(GravityCompat.START);
-      }
-      if (!viewIsAtHome) { //if the current view is not the Home fragment
-         displayView(R.id.nav_recordActivity); //display the Home fragment
+      if (getSupportFragmentManager().getBackStackEntryCount() == 1) {
+         finish();
       } else {
-         moveTaskToBack(true);  //If view is in Record Activity fragment, exit application
+         super.onBackPressed();
       }
    }
 
@@ -73,37 +151,37 @@ public class MainActivity extends AppCompatActivity
       switch (viewId) {
          case R.id.nav_accountStatistics:
             fragment = new AccountStatisticsFragment();
-            title  = ((AccountStatisticsFragment)fragment).getTitle();
+            title = ((AccountStatisticsFragment) fragment).getTitle();
             viewIsAtHome = false;
             break;
 
          case R.id.nav_achievements:
             fragment = new AchievementsFragment();
-            title = ((AchievementsFragment)fragment).getTitle();
+            title = ((AchievementsFragment) fragment).getTitle();
             viewIsAtHome = false;
             break;
 
          case R.id.nav_accountDetails:
             fragment = new AccountDetailsFragment();
-            title = ((AccountDetailsFragment)fragment).getTitle();
+            title = ((AccountDetailsFragment) fragment).getTitle();
             viewIsAtHome = false;
             break;
 
          case R.id.nav_trailMap:
             fragment = new TrailMapFragment();
-            title = ((TrailMapFragment)fragment).getTitle();
+            title = ((TrailMapFragment) fragment).getTitle();
             viewIsAtHome = false;
             break;
 
          case R.id.nav_activityHistory:
             fragment = new ActivityHistoryFragment();
-            title = ((ActivityHistoryFragment)fragment).getTitle();
+            title = ((ActivityHistoryFragment) fragment).getTitle();
             viewIsAtHome = false;
             break;
 
-        case R.id.nav_recordActivity:
+         case R.id.nav_recordActivity:
             fragment = new RecordActivityFragment();
-            title = ((RecordActivityFragment)fragment).getTitle();
+            title = ((RecordActivityFragment) fragment).getTitle();
             viewIsAtHome = true;
             break;
 
@@ -115,11 +193,8 @@ public class MainActivity extends AppCompatActivity
             break;
       }
 
-      // Changes the current fragment
-      if (fragment != null) {
-         FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
-         ft.replace(R.id.frameLayoutContent, fragment);
-         ft.commit();
+      if(fragment != null){
+         replaceFragment(fragment);
       }
 
       // set the toolbar title
@@ -135,20 +210,16 @@ public class MainActivity extends AppCompatActivity
    @Override
    public boolean onNavigationItemSelected(MenuItem item) {
 
-      if(currentMenuItem == null)
-      {
+      if (currentMenuItem == null) {
          currentMenuItem = item;
       }
 
       displayView(item.getItemId());
 
       // Don't select Exit Item since it doesn't relate to the current fragment being displayed
-      if(item.getTitle().equals("Exit"))
-      {
+      if (item.getTitle().equals("Exit")) {
          currentMenuItem.setChecked(true);
-      }
-      else
-      {
+      } else {
          currentMenuItem = item;
       }
 
@@ -156,7 +227,7 @@ public class MainActivity extends AppCompatActivity
    }
 
    // The Alert for Exit Navigation drawer Item
-   private void initializeExitAlert(){
+   private void initializeExitAlert() {
       builder = new AlertDialog.Builder(this);
 
       // Set the properties
