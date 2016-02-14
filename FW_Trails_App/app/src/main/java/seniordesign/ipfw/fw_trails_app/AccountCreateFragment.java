2a3d1052a78836398c467c4554cad41698334e50
Jaron Somers <somejj01@students.ipfw.edu>
2016-02-13 19:08:08 -0500

    Implementing Create Account Fragment:

--- FW_Trails_App/app/src/main/java/seniordesign/ipfw/fw_trails_app/AccountCreateFragment.java
+++ FW_Trails_App/app/src/main/java/seniordesign/ipfw/fw_trails_app/AccountCreateFragment.java
@@ -4,9 +4,11 @@ import android.content.Context;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.RelativeLayout;
 
 
 /**
@@ -18,90 +20,30 @@ import android.view.ViewGroup;
  * create an instance of this fragment.
  */
 public class AccountCreateFragment extends Fragment {
-   // TODO: Rename parameter arguments, choose names that match
-   // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-   private static final String ARG_PARAM1 = "param1";
-   private static final String ARG_PARAM2 = "param2";
 
-   // TODO: Rename and change types of parameters
-   private String mParam1;
-   private String mParam2;
+   private final String fragmentTitle = "Create Account";
 
-   private OnFragmentInteractionListener mListener;
-
-
-   /**
-    * Use this factory method to create a new instance of
-    * this fragment using the provided parameters.
-    *
-    * @param param1 Parameter 1.
-    * @param param2 Parameter 2.
-    * @return A new instance of fragment AccountCreateFragment.
-    */
-   // TODO: Rename and change types and number of parameters
-   public static AccountCreateFragment newInstance(String param1, String param2) {
-      AccountCreateFragment fragment = new AccountCreateFragment();
-      Bundle args = new Bundle();
-      args.putString(ARG_PARAM1, param1);
-      args.putString(ARG_PARAM2, param2);
-      fragment.setArguments(args);
-      return fragment;
-   }   public AccountCreateFragment() {
-      // Required empty public constructor
-   }
-
-   @Override
-   public void onCreate(Bundle savedInstanceState) {
-      super.onCreate(savedInstanceState);
-      if (getArguments() != null) {
-         mParam1 = getArguments().getString(ARG_PARAM1);
-         mParam2 = getArguments().getString(ARG_PARAM2);
-      }
-   }
-
-   @Override
    public View onCreateView(LayoutInflater inflater, ViewGroup container,
                             Bundle savedInstanceState) {
-      // Inflate the layout for this fragment
-      return inflater.inflate(R.layout.fragment_account_create, container, false);
+      FragmentActivity faActivity  = (FragmentActivity)    super.getActivity();
+      // Replace LinearLayout by the type of the root element of the layout you're trying to load
+      RelativeLayout loadedRelativeLayout    = (RelativeLayout)    inflater.inflate(R.layout.fragment_account_create, container, false);
+      // Of course you will want to faActivity and llLayout in the class and not this method to access them in the rest of
+      // the class, just initialize them here
+
+      // Content of previous onCreate() here
+      // ...
+      // Don't use this method, it's handled by inflater.inflate() above :
+      // setContentView(R.layout.activity_layout);
+
+      // The FragmentActivity doesn't contain the layout directly so we must use our instance of     LinearLayout :
+      // loadedRelativeLayout.findViewById(R.id.someGuiElement);
+      // Instead of :
+      // findViewById(R.id.someGuiElement);
+      return loadedRelativeLayout; // We must return the loaded Layout
    }
 
-   // TODO: Rename method, update argument and hook method into UI event
-   public void onButtonPressed(Uri uri) {
-      if (mListener != null) {
-         mListener.onFragmentInteraction(uri);
-      }
-   }
-
-   @Override
-   public void onAttach(Context context) {
-      super.onAttach(context);
-      if (context instanceof OnFragmentInteractionListener) {
-         mListener = (OnFragmentInteractionListener) context;
-      } else {
-         throw new RuntimeException(context.toString()
-                 + " must implement OnFragmentInteractionListener");
-      }
-   }
-
-   @Override
-   public void onDetach() {
-      super.onDetach();
-      mListener = null;
-   }
-
-   /**
-    * This interface must be implemented by activities that contain this
-    * fragment to allow an interaction in this fragment to be communicated
-    * to the activity and potentially other fragments contained in that
-    * activity.
-    * <p/>
-    * See the Android Training lesson <a href=
-    * "http://developer.android.com/training/basics/fragments/communicating.html"
-    * >Communicating with Other Fragments</a> for more information.
-    */
-   public interface OnFragmentInteractionListener {
-      // TODO: Update argument type and name
-      void onFragmentInteraction(Uri uri);
+   public String getTitle(){
+      return fragmentTitle;
    }
 }
