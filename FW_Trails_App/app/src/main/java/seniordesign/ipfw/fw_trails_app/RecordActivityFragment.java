3e19880f7759dd7c3d4a79f36cac433c7e0ffa2b
zeek55555 <zeek55wb@gmail.com>
2016-02-21 11:58:57 -0500

    Implemented activity type selection dialog in recording activity fragment

--- FW_Trails_App/app/src/main/java/seniordesign/ipfw/fw_trails_app/RecordActivityFragment.java
+++ FW_Trails_App/app/src/main/java/seniordesign/ipfw/fw_trails_app/RecordActivityFragment.java
@@ -1,21 +1,20 @@
 package seniordesign.ipfw.fw_trails_app;
 
 import android.Manifest;
+import android.content.DialogInterface;
 import android.content.pm.PackageManager;
 import android.graphics.Color;
 import android.location.Location;
 import android.os.Bundle;
-import android.os.Handler;
 import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentActivity;
 import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AlertDialog;
 import android.util.Log;
 import android.view.InflateException;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
-import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 import com.google.android.gms.common.ConnectionResult;
@@ -292,9 +291,37 @@ public class RecordActivityFragment extends Fragment implements OnMapReadyCallba
 
         @Override
         public void onClick(View v) {
-            startRecording();
+            selectActivityType();
+//            startRecording();
         }
     };
+
+    public void selectActivityType(){
+        CharSequence colors[] = new CharSequence[] {"Biking", "Running", "Walking", "Skateboarding", "Skating"};
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(getChildFragmentManager().findFragmentById(R.id.map).getContext());
+        builder.setTitle("Pick a color");
+        builder.setItems(colors, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                switch(which) {
+                    case 0: startRecording();
+                        break;
+                    case 1: startRecording();
+                        break;
+                    case 2: startRecording();
+                        break;
+                    case 3: startRecording();
+                        break;
+                    case 4: startRecording();
+                        break;
+                    default: startRecording();
+                        break;
+                }
+            }
+        });
+        builder.show();
+    }
 
     public void startRecording() {
         recording = true;
