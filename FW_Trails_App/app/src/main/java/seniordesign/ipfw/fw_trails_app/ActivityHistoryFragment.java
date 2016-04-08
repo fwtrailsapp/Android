365a1b5c03d823943210fe5a8ecf9d2366e50bbf
Jaron Somers <somejj01@students.ipfw.edu>
2016-04-07 20:43:55 -0400

    Refactored the ActivityHistoryController to parse the returned activity histories correctly and adjusted the column alignments to fit better on the screen

--- FW_Trails_App/app/src/main/java/seniordesign/ipfw/fw_trails_app/ActivityHistoryFragment.java
+++ FW_Trails_App/app/src/main/java/seniordesign/ipfw/fw_trails_app/ActivityHistoryFragment.java
@@ -1,10 +1,12 @@
 package seniordesign.ipfw.fw_trails_app;
 
+import android.content.DialogInterface;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.os.Looper;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
+import android.support.v7.app.AlertDialog;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -31,12 +33,14 @@ public class ActivityHistoryFragment extends Fragment {
    ArrayList<ActivityHistoryModel> items;
    private ListView listView;
    private ActivityHistoryListViewAdapter adapter;
+   private View view;
+
    public View onCreateView(LayoutInflater inflater, ViewGroup container,
                             Bundle savedInstanceState) {
       FragmentActivity faActivity  = (FragmentActivity)    super.getActivity();
 
       // Replace LinearLayout by the type of the root element of the layout you're trying to load
-      View view = inflater.inflate(R.layout.fragment_activity_history, container, false);
+      view = inflater.inflate(R.layout.fragment_activity_history, container, false);
 
       listView = (ListView) view.findViewById(R.id.activity_history_listView);
 
@@ -55,6 +59,24 @@ public class ActivityHistoryFragment extends Fragment {
       return fragmentTitle;
    }
 
+   // Displays a dialog describing the error passed in.
+   private void displayError(String errorText) {
+
+      AlertDialog alertDialog = new AlertDialog.Builder(getContext()).create();
+      alertDialog.setTitle(fragmentTitle+ " Error");
+
+      // Modal settings are set
+      alertDialog.setCancelable(false);
+      alertDialog.setMessage(errorText);
+      alertDialog.setButton(AlertDialog.BUTTON_POSITIVE, "OK",
+              new DialogInterface.OnClickListener() {
+                 public void onClick(DialogInterface dialog, int which) {
+                    dialog.dismiss();
+                 }
+              });
+
+      alertDialog.show();
+   }
 
    /*
    The ActivityHistoryController class.
@@ -92,16 +114,16 @@ public class ActivityHistoryFragment extends Fragment {
                   ExerciseType exercise;
 
                   // Convert the single response to a JSON Object
-                  JSONObject jsonResponse = new JSONObject(new String(response));
+                  JSONArray activityHistoryJSON = new JSONArray(new String(response));
 
                   // Convert the single JSON Object to an array
-                  JSONArray activityHistory = jsonResponse.getJSONArray("GetActivitiesForUserResult");
+
 
                   // Loop through all the returned activities and create models based on them
-                  for (int i = 0; i < activityHistory.length(); i++) {
+                  for (int i = 0; i < activityHistoryJSON.length(); i++) {
 
                      // Get the single JSON Object aka 1 activity history model
-                     activity = activityHistory.getJSONObject(i);
+                     activity = activityHistoryJSON.getJSONObject(i);
                      exerciseType = activity.getString("exercise_type");
                      theDuration = new Duration(activity.getString("duration"));
 
@@ -119,8 +141,17 @@ public class ActivityHistoryFragment extends Fragment {
             }
 
             @Override
-            public void onFailure(int statusCode, Header[] headers, byte[] errorResponse, Throwable e) {
+            public void onFailure(int statusCode, Header[] headers, final byte[] errorResponse, Throwable e) {
                // called when response HTTP status is "4XX" (eg. 401, 403, 404)
+               // Run on UI thread due to AsyncTask
+
+               getActivity().runOnUiThread(new Runnable() {
+
+                  public void run() {
+                     String errorText = "There was a problem getting your activity history.";
+                     displayError(errorText);
+                  }
+               });
             }
 
          });
@@ -135,9 +166,11 @@ public class ActivityHistoryFragment extends Fragment {
          super.onPostExecute(theItems);
 
          // Bind data to the custom ListViewAdapter
-         adapter = new ActivityHistoryListViewAdapter(getContext(),R.layout.activity_history_row,R.id.durationText,theItems);
+         if(theItems != null){
+            adapter = new ActivityHistoryListViewAdapter(getContext(),R.layout.activity_history_row,R.id.durationText,theItems);
 
-         listView.setAdapter(adapter);
+            listView.setAdapter(adapter);
+         }
       }
 
       // Determines the exercise type based on a string received from the server's
